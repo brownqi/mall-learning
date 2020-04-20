f4334e7400bda05e4765b5429f2dd3fba4a532b5
BrownQi <36023900+BrownQi@users.noreply.github.com>
2020-04-20 22:09:14 +0800

    项目初始结构的创建

---
+++ mall-ware/src/test/java/cn/brownqi/mall/ware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.brownqi.mall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
